b05f90f191f9bb769e872f9c140f5c33f094a900
Hikmal17 <hkmlysf7@gmail.com>
2024-06-24 17:46:02 +0800

    holeofdamascus

---
+++ berita/models.py
@@ -0,0 +1,44 @@
+from ckeditor_uploader.fields import RichTextUploadingField
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils.text import slugify
+import datetime
+
+class Kategori(models.Model):
+    nama = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nama
+
+    class Meta:
+        verbose_name_plural = "1. Kategori"
+
+class Artikel(models.Model):
+    judul = models.CharField(max_length=225)
+    isi = RichTextUploadingField(
+        config_name='special',
+        external_plugin_resources=[(
+            'youtube',
+            'http://localhost:8000/static/ckeditor_plugins/youtube/youtube/',
+            'plugin.js',
+        )],
+        blank=True,
+        null=True
+    )
+    kategori = models.ForeignKey(Kategori, on_delete=models.SET_NULL, blank=True, null=True)
+    author = models.ForeignKey(User, on_delete=models.PROTECT)
+    thumbnail = models.ImageField(upload_to='artikel', blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    slug = models.SlugField(max_length=255, unique=True, blank=True, null=True)
+
+    def __str__(self):
+        return self.judul
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            current_time = datetime.datetime.now()
+            self.slug = slugify(f"{current_time.year}-{current_time.month}-{current_time.day}-{self.judul}")
+        super(Artikel, self).save(*args, **kwargs)
+
+    class Meta:
+        verbose_name_plural = "2. Artikel"
---
+++ myproject/authentikasi.py
@@ -0,0 +1,62 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout 
+from django.contrib.auth.models import User
+def akun_login(request):
+    if request.user.is_authenticated:
+        return redirect('/')
+    template_name = "halaman/login.html"
+    pesan = ''
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        user = authenticate(request, username=username, password=password)
+        if user:
+                login(request, user)    
+                return redirect('/')
+        else :
+            pesan = "Gagal Login"
+    context = {
+        'pesan' : pesan
+    }
+    return render(request, template_name, context)
+
+def akun_registrasi(request):
+    if request.user.is_authenticated:
+        return redirect('/')
+    pesan = ''
+    template_name = "halaman/registrasi.html"
+    if request.method == "POST":
+        username = request.POST.get("username")
+        first_name = request.POST.get("first_name")
+        last_name = request.POST.get("last_name")
+        email = request.POST.get("email")
+        password1 = request.POST.get("password1")
+        password2 = request.POST.get("password2")
+        
+        if password1 == password2:
+            check_user = User.objects.filter(username=username)
+            if check_user.count() == 0:
+                user_simpan = User.objects.create(
+                    username = username,
+                    first_name = first_name,
+                    last_name = last_name,
+                    email = email,
+                    # password = password1,
+                    is_active = True
+                )
+                user_simpan.set_password(password1)
+                user_simpan.save()
+                return redirect('/')
+            else:
+                pesan = "Username Sudah Digunakan"
+        else :
+            pesan = "Password Tidak Sama"
+    
+    context = {
+        'pesan' : pesan
+    }
+    return render(request, template_name, context)
+
+def akun_logout(request):
+    logout(request)
+    return redirect('/')
